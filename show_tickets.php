a999aa33a4874687b24685fd4dc96424e509fb0d
Luis <lalo_luis_6@hotmail.com>
2021-07-16 21:16:33 -0500

    Web responsiva

--- show_tickets.php
+++ show_tickets.php
@@ -3,39 +3,40 @@
 
 <div class="container">
     <div class="row">
-        <div class="col-8 mx-auto mt-4">
+        <div class="col-lg-8 col-md-10 col-sm mx-auto mt-4">
             <div class="col mb-3">
                 <h1>Tickets Guardados</h1>
             </div>
 
             <!-- Tabla con el contenido de la base de datos -->
-            <table class="table table-hover">
-                <thead>
-                    <tr>
-                        <!-- <th>Id</th> -->
-                        <th>Ticket</th>
-                        <th>Fecha y Hora</th>
-                        <th>Opciones</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php
-                        $query = "SELECT * FROM Tickets";
-                        $result = mysqli_query( $conn, $query );
-
-                        while ($row = mysqli_fetch_array($result)) { ?>
-                            <tr>
-                                <!-- <td><?= $row['id']; ?></td> -->
-                                <td><?= $row['content']; ?></td>
-                                <td><?= $row['creation_date']; ?></td>
-                                <td>
-                                        <a href="delete_ticket.php?id= <?= $row['id'] ?>" class="btn btn-danger">Borrar</a>
-                                        <a href="edit_ticket.php?id= <?= $row['id'] ?>" class="btn btn-info">Editar</a>
-                                </td>
-                            </tr>
-                    <?php } ?>
-                </tbody>
-            </table>
+            <div class="table-responsive-sm table-responsive-md">
+                <table class="table table-hover">
+                    <thead>
+                        <tr>
+                            <!-- <th>Id</th> -->
+                            <th>Ticket</th>
+                            <th>Fecha y Hora</th>
+                            <th>Opciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                            $query = "SELECT * FROM Tickets";
+                            $result = mysqli_query( $conn, $query );
+                            while ($row = mysqli_fetch_array($result)) { ?>
+                                <tr>
+                                    <!-- <td><?= $row['id']; ?></td> -->
+                                    <td><?= $row['content']; ?></td>
+                                    <td><?= $row['creation_date']; ?></td>
+                                    <td>
+                                            <a href="delete_ticket.php?id= <?= $row['id'] ?>" class="btn btn-danger">Borrar</a>
+                                            <a href="edit_ticket.php?id= <?= $row['id'] ?>" class="btn btn-info">Editar</a>
+                                    </td>
+                                </tr>
+                        <?php } ?>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 </div>
